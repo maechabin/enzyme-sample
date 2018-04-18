e9917998bb9347b557d9be775ba2b0c32a21e883
前田貴範 <maedapc@maedatakanorinoMacBook-Pro.local>
2018-04-18 19:17:30 +0900

    追加

--- src/App.test.js
+++ src/App.test.js
@@ -1,39 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App, Title, Input, Button } from './App';
+import renderer from 'react-test-renderer';
 
-import  { shallow } from 'enzyme';
+import  { shallow, mount } from 'enzyme';
 
 
 describe('<App />', () => {
+  const wrapper = shallow(<App />);
+
   test('', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(Title).length).toBe(1);
     expect(wrapper.find(Input).length).toBe(1);
     expect(wrapper.find(Button).length).toBe(1);
   });
+
+  test('スナップショット', () => {
+    const tree = renderer
+      .create(<App />)
+      .toJSON();
+    expect(tree).toMatchSnapshot('aa');
+  });
+
+  test('setState', () => {
+    wrapper.setState({
+      text: 'aaa',
+    });
+    expect(wrapper.find('.aaa').length).toBe(1);
+  });
 });
 
 describe('<Title />', () => {
-  it('', () => {
-    const wrapper = shallow(<Title title={'aaa'} />);
-    expect(wrapper.text()).toBe('aaa');
-    wrapper.setProps({ title: 'bar' });
-    expect(wrapper.text()).toBe('bar');
+  test('', () => {
+    const wrapper = shallow(<Title text={'React'} />);
+    expect(wrapper.text()).toBe('Hello React');
+    wrapper.setProps({ text: 'World' });
+    expect(wrapper.text()).toBe('Hello World');
   });
 });
 
 describe('<Input />', () => {
-  it('', () => {
+  test('', () => {
     const wrapper = shallow(<Input />);
     expect(wrapper.find('input').length).toBe(1);
   });
 
-  it('', () => {
-    const handleChange = jest.fn();
-    const wrapper = shallow(<Input />);
+  test('', () => {
+    const handleChangeSpy = jest.fn();
+    const wrapper = mount(<Input handleChange={handleChangeSpy} />);
+
     const event = { target: { value: 'aaa' } };
     wrapper.find('input').simulate('change', event);
-    expect(handleChange.mock.calls.length).toBe(1);
+    expect(handleChangeSpy).toHaveBeenCalledWith('aaa');
   });
+
 });
+
+describe('<Button />', () => {
+  test('', () => {
+    const wrapper = shallow(<Button />);
+    expect(wrapper.find('button').length).toBe(1);
+  });
+  
+  test('', () => {
+    const handleClickSpy = jest.fn();
+    const wrapper = shallow(<Button handleClick={handleClickSpy} />);
+    wrapper.find('button').simulate('click');
+    expect(handleClickSpy).toHaveBeenCalled();
+  })
+});
+
